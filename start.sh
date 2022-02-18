623375eda2e6d696850661bd5f8134ce93152862
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2022-02-18 20:22:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/DalinMathew/Wanda-Maximoff-Bot /Wanda-Maximoff-Bot
+  git clone https://github.com/DalinMathew/Wanda-Maximoff-Bot   /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/DalinMathew/Wanda-Maximoff-Bot -b $BRANCH /Wanda-Maximoff-Bot
+  git clone https://github.com/DalinMathew/Wanda-Maximoff-Bot -b $BRANCH /VCPlayerBot
 fi
-cd /Wanda-Maximoff-Bot
+cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
